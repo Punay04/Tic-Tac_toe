ff31dfa32d0e0fb6372a5db7aa18a0349275235b
surFer_04 <kukrejapunay04@gmail.com>
2024-11-10 03:35:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let currentPlayer = "X";
+let arr = Array(9).fill(null);
+
+function checkWinner() {
+  if (
+    (arr[0] !== null && arr[0] === arr[1] && arr[1] === arr[2]) ||
+    (arr[3] !== null && arr[3] === arr[4] && arr[4] === arr[5]) ||
+    (arr[6] !== null && arr[6] === arr[7] && arr[7] === arr[8]) ||
+    (arr[0] !== null && arr[0] === arr[3] && arr[3] === arr[6]) ||
+    (arr[1] !== null && arr[1] === arr[4] && arr[4] === arr[7]) ||
+    (arr[2] !== null && arr[2] === arr[5] && arr[5] === arr[8]) ||
+    (arr[0] !== null && arr[0] === arr[4] && arr[4] === arr[8]) ||
+    (arr[2] !== null && arr[2] === arr[4] && arr[4] === arr[6])
+  ) {
+    document.write(`Winner is : ${currentPlayer}`);
+    return;
+  }
+  if (
+    !arr.some(function (e) {
+      return e === null;
+    })
+  ) {
+    document.write("The game is draw");
+    return;
+  }
+}
+
+function handleClick(el) {
+  const id = Number(el.id);
+  if (arr[id] !== null) {
+    return;
+  }
+  arr[id] = currentPlayer;
+  el.innerText = currentPlayer;
+  checkWinner();
+  if (currentPlayer === "X") {
+    currentPlayer = "O";
+  } else {
+    currentPlayer = "X";
+  }
+}
